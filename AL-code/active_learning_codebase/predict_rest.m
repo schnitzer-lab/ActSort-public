b93d1c5e5527cdf047571e09ac4f9b8fac900a73
Yiqi Jiang <47072150+YiqiJ@users.noreply.github.com>
2025-03-07 13:23:35 -0800

    update dataset structure and predict_rest in AL

--- AL-code/active_learning_codebase/predict_rest.m
+++ AL-code/active_learning_codebase/predict_rest.m
@@ -1,19 +1,32 @@
-function [labels] = predict_rest(dataset)
+function [labels, stats] = predict_rest(dataset)
 % This function will predict all the unlabeled dataset with the latest cell
 % sorter
 % INPUT
 %  [dataset] : a structure that contain fields
-%       - features : N x d
-%       - labels_ex : N x 1 human / expert labels
-%       - labels_ml : N x 1 cell classifier / ML labels 
+%       - features  : cell of N x d
+%       - labels_ex : cell of N x 1 human / expert labels
+%       - labels_ml : cell of N x 1 cell classifier / ML labels 
 % OUTPUT
 %  [labels] : expert labels complemented with cell classifier's prediction
-%             on all unlabeled data (N x 1)
-labels_ml = dataset.labels_ml;
-labels_ex = dataset.labels_ex;
-zeroIndices = dataset.labels_ex == 0;
+%             on all unlabeled data (cell of N x 1)
+%  [stats]  : a cell of stats for each dataset, including the # of sorted 
+%             good cells, # of sorted bad cells, and the # of unlabeled
+%             cells in each individual datasets_i. 
+labels       = cell(1, dataset.num_datasets);
+stats      = cell(1, dataset.num_datasets);
 
-% Replace zeros in labels_ex with corresponding values from labels_ml
-labels_ex(zeroIndices) = labels_ml(zeroIndices);
-labels = labels_ex;
+for i=1:dataset.num_datasets
+    labels_ml = dataset.labels_ml{i};
+    labels_ex = dataset.labels_ex{i};
+    zeroIndices = dataset.labels_ex{i} == 0;
+    
+    % Replace zeros in labels_ex with corresponding values from labels_ml
+    labels_ex(zeroIndices) = labels_ml(zeroIndices);
+    labels{i} = labels_ex;
+
+    numGood      = sum(labels_ex == 1);
+    numBad       = sum(labels_ex == -1);
+    numUnlabeled = sum(labels_ex == 0);
+    stats{i}     = [numGood, numBad, numUnlabeled];
+end
 end
